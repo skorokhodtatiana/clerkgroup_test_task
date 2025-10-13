fa2a7da2be1f307dd137db7db9c5ef6385a83151
tskorohod <tskorohod@lmteam.by>
2025-10-13 16:55:20 +0300

    changed component

--- test_task/src/components/step/Step.jsx
+++ test_task/src/components/step/Step.jsx
@@ -1,16 +1,24 @@
-// import Button from '../button/button';
 import * as Icons from '../icons/step/Icons-step';
 import './Step.scss';
 
-const Step = ({src, alt}) => {
+const Step = ({src, alt, text, lastChild=""}) => {
 
 	return (
-		<>
-			<div className="step-icon">
-				<img className="step-icon__circle" src={Icons.circle} alt="circle"></img>
+		<div className="step-icon">
+			<div></div>
+			<div className="step-icon__image">
+				<img src={Icons.circle} alt="circle"></img>
 				<img className="step-icon__icon" src={src} alt={alt}></img>
 			</div>
-		</>
+			<div className={"step-icon__dots " + lastChild}>
+				<div className="step-icon__dot"></div>
+				<div className="step-icon__dot"></div>
+				<div className="step-icon__dot"></div>
+				<div className="step-icon__dot"></div>
+				<div className="step-icon__dot"></div>
+			</div>
+			<div className="step-icon__text">{ text }</div> 
+		</div>
 	)
 }
 
