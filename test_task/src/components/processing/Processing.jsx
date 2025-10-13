ba3e87608822defa3a50dedecf993c3248db6b31
tskorohod <tskorohod@lmteam.by>
2025-10-13 16:54:43 +0300

    small changes styles,

--- test_task/src/components/processing/Processing.jsx
+++ test_task/src/components/processing/Processing.jsx
@@ -13,11 +13,11 @@ const Processing = () => {
 				</h2>
 				<div className="processing__title-second">Перед заполнением формы ознакомьтесь с нашей схемой работы!</div>
 				<div className="processing__steps">
-					<Step src={ Icons.search } alt={"Icons.search"} text={"Lorem ipsum dolorsit amet"}></Step>
+					<Step src={ Icons.search } alt={"Icons.search"} text="Lorem ipsum dolorsit amet"></Step>
 					<Step src={ Icons.percent } alt={"Icons.percent"} text="Сonsecteturadipiscing elit"></Step>
 					<Step src={ Icons.file } alt={"Icons.file"} text="Sed do eiusmod tempor"></Step>
 					<Step src={ Icons.message } alt={"Icons.message"} text="Esse cillum dolore eu fugiat"></Step>
-					<Step src={ Icons.money } alt={"Icons.money"} lastChild="last-item"  text="Excepteur sint occaecat cupidatat non proident"></Step>
+					<Step src={ Icons.money } alt={"Icons.money"} lastChild="last-item" text="Excepteur sint occaecat cupidatat non proident"></Step>
 				</div>
 				<div>
 					<Form></Form>
