97b2e505355993d7e16bdc8fe8d91862f3cb068f
tskorohod <tskorohod@lmteam.by>
2025-10-13 23:38:00 +0300

    added progress bar og input type file

--- test_task/src/components/form/Form.jsx
+++ test_task/src/components/form/Form.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Input from '../input/Input';
 import Button from '../button/button';
-import upload from '../../assets/images/processing/upload.svg';
+import upload from '@/assets/images/processing/upload.svg';
 import './Form.scss';
 
 const Form = () => {
-	//const [systemValue, setSystemValue] = useState('');
+	const [systemValue, setSystemValue] = useState('Выберите тип системы');
 	const [emailValue, setEmailValue] = useState('');
 	const [nameValue, setNameValue] = useState('');
+	const [percentValue, setPercent] = useState(0);
 	const [fileValue, setFileValue] = useState('');
+
+	const uploadRef = useRef();
+	const statusRef = useRef();
+	const progressRef = useRef();
+
+	const UploadFile = () => {
+		setFileValue(fileValue);
+		var formData = new FormData();
+		formData.append("file", fileValue);
+		var xhr = new XMLHttpRequest();
+		xhr.upload.addEventListener("progress", ProgressHandler, false);
+		xhr.open("POST", "some path");
+		xhr.send(formData);
+	};
+
+	const ProgressHandler = (e) => {
+		var percent = (e.loaded / e.total) * 100;
+		statusRef.current.innerHTML = Math.round(percent) + "% uploaded...";
+		setPercent(percent)
+		progressRef.current.value = Math.round(percent);
+	};
 
 	const arrItemForSelect = [
 		'Sed ut perspiciatis',
@@ -21,10 +43,10 @@ const Form = () => {
 	return (
 		<>
 			<form className="form" method="post" encType="multipart/form-data">
-				<select name="typeSystem" className="form__select">
-					<option key="" value="Выберите тип системы" selected disabled>Выберите тип системы</option>
+				<select name="typeSystem" className="form__select" onChange={(e) => setSystemValue(e.target.value)} value={systemValue}>
+					<option key="" value="Выберите тип системы" disabled>Выберите тип системы</option>
 					{arrItemForSelect.map((item, index) => (
-						<option key={ index } value={item}>{ item }</option>
+						<option key={ index } name={item}>{ item }</option>
 					))}
 					<option></option>
 				</select>
@@ -33,15 +55,15 @@ const Form = () => {
 				<div className="form__progress-block">
 					<div className="form__progress-text">
 						<span>Sed ut perspiciatis, unde omnis iste natus</span>
-						<span>75%</span>
+						<span className="form__progress-percent" ref={statusRef}>{percentValue + "%"}</span>
 					</div>
-					<progress className="form__progress" id="progressBar" value="0" max="100"></progress>
+					<progress className="form__progress " ref={progressRef} id="progressBar" value="0" max="100"></progress>
 				</div>
 				<label className="form__label-upload-file" htmlFor="file-upload">
 					<img className="form__label-img" src={upload} alt="upload"></img>
 					<span className="form__label-text">ПРИКРЕПИТЬ ФАЙЛ</span>
 				</label>
-				<Input inputClass="form__upload-file" id="file-upload" name="file-upload" type="file" accept=".pdf" placeholder="Прикрепить файл" value={fileValue} onChange={(e) => setFileValue(e.target.value)}></Input>
+				<Input inputClass="form__upload-file" ref={uploadRef} id="file-upload" name="file-upload" type="file" placeholder="Прикрепить файл" value={fileValue} onChange={UploadFile}></Input>
 				<Button type="submit" className={'processing__button'} children={'ОТПРАВИТЬ'}></Button>
 			</form>
 		</>
