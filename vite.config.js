0f9471fd4840cde1525f64a4243d8d4bbb0bbb7a
Tatiana Skorokhod <74983796+skorokhodtatiana@users.noreply.github.com>
2025-10-14 13:18:46 +0300

    Update vite.config.js
    changed name repository

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/clerkgroup.github.io',
+  base: '/clerkgroup_test_task',
   resolve: {
     alias: {
       '@': path.resolve('./src'),
