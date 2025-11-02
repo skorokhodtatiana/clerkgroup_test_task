ef8f81841f2d5979ac61906ec3d05799a7a3b9ef
tskorohod <tskorohod@lmteam.by>
2025-11-02 20:07:05 +0300

    made adaptation of header for mobile devices

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import './Nav.scss';
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+	const [isMobile, setIsMobile] = useState(false);
+	const handleClick = () => {
+		setIsMobile(!isMobile)
+	}
+
 	return (
-		<>
-			<nav className="nav">
-				<NavLink className="nav__link nav__link_blue" to="/">БИЗНЕС</NavLink>
+		<div className="nav" >
+			<div className={isMobile ? "burger-menu burger-menu_open" : "burger-menu"} onClick={handleClick}>
+				<span className="burger-menu__line"></span>
+				<span className="burger-menu__line"></span>
+				<span className="burger-menu__line"></span>
+			</div>
+			<nav className={isMobile ? "nav__items nav__items_flex" : "nav__items"}>
+				<NavLink className="nav__link" to="/">БИЗНЕС</NavLink>
 				<NavLink className="nav__link" to="/aboutUs">О НАС</NavLink>
 				<NavLink className="nav__link" to="/error">ЦЕНЫ</NavLink>
 				<a className="nav__link" href="/#">ОФОРМИТЬ ЗАКАЗ</a>
 			</nav>
-		</>
+		</div>
 	)
 }
 
