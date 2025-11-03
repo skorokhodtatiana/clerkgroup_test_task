b37948633d24396c1c11357acf2fbd53c22494e5
tskorohod <tskorohod@lmteam.by>
2025-11-03 13:46:11 +0300

    made adaptation of footer for mobile devices

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,37 @@
-
 import * as Icons from '../icons/footer/Icons';
 import './Footer.scss';
 
 const Footer = () => {
 	return (
-		<div className="footer-container">
+		<>
 			<div className="footer">
 				<div className="footer__author">© 2018 «LOREMIPSUM.NET» Все права защищены.</div>
-				<div className="footer__verticle-line"></div>
-				<div className="footer__item">
-					<img className="footer__item-img" src={Icons.wallet} alt="walet"></img>
-					QIWI wallet
+				<div className="footer__options-payment">
+					<div className="footer__item">
+						<img src={Icons.wallet} alt="walet"></img>
+						<span className="footer__item-text">QIWI wallet</span>
+					</div>
+					<div className="footer__item">
+						<img src={Icons.yandex} alt="yandex"></img>
+						<span className="footer__item-text">Yandex Money</span>
+					</div>
+					<div className="footer__item">
+						<img src={Icons.web} alt="web"></img>
+						<span className="footer__item-text">Web Money</span>
+					</div>
 				</div>
-				<div className="footer__item">
-					<img className="footer__item-img" src={Icons.yandex} alt="yandex"></img>
-					Yandex Money
+				<div className="footer__options-payment">
+					<a className="footer__link" href="mailto:info@hexlet.io">
+						<img src={Icons.mail} alt="mail"></img>
+						<span className="footer__item-text">info@ipsum228.com</span>
+					</a>
+					<a className="footer__link" href="mailto:info@hexlet.io">
+						<img src={Icons.vk} alt="vk"></img>
+						<span className="footer__item-text">Мы вконтакте</span>
+					</a>
 				</div>
-				<div className="footer__item">
-					<img className="footer__item-img" src={Icons.web} alt="web"></img>
-					Web Money
-				</div>
-				<div className="footer__verticle-line"></div>
-				<a className="footer__link" href="mailto:info@hexlet.io">
-					<img className="footer__item-img" src={Icons.mail} alt="mail"></img>
-					info@ipsum228.com
-				</a>
-				<a className="footer__link" href="mailto:info@hexlet.io">
-					<img className="footer__item-img" src={Icons.vk} alt="vk"></img>
-					Мы вконтакте
-				</a>
 			</div>
-		</div>
+		</>
 	)
 }
 
